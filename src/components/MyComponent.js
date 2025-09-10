d5048b2431f5cfdcc4673386a26bef2ef5a239df
Minh Dương <dnminh201103@gmail.com>
2025-09-10 20:17:21 +0700

    video 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,15 @@
+// 2 cách viết component
+// class component
+// function component
+import React from "react";
+
+class MyComponent extends React.Component {
+    render() {
+        return(
+            <div>my first component {Math.random()}
+            </div>
+        );
+    }
+} 
+
+export default MyComponent;
