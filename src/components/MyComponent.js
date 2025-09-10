667d02bff60fee481f34a9ec2571b74f1be9aecb
Minh Dương <dnminh201103@gmail.com>
2025-09-10 20:24:52 +0700

    video 16

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,11 +5,11 @@ import React from "react";
 
 class MyComponent extends React.Component {
     render() {
-        return(
-            <div>my first component {Math.random()}
+        return (
+            <div>
+                my first component {Math.random()}
             </div>
         );
     }
-} 
-
+}
 export default MyComponent;
