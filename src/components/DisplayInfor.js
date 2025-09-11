7fe1cf9e7d9ba1b329ec3582624775a4d2f9e266
Minh Dương <dnminh201103@gmail.com>
2025-09-11 21:54:50 +0700

    video 28

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,5 +1,6 @@
 import React, { use } from "react";
 import './DisplayInfor.scss';
+import logo from './../logo.svg'
 
 class DisplayInfor extends React.Component {
 
@@ -20,13 +21,14 @@ class DisplayInfor extends React.Component {
         // props => viết tắt của properties 
         return (
             <div className="display-info-container">
+                <img src={logo} />
                 {this.state.isShowListUser &&
                     <div>
                         {listUser.map((user, index) => {
                             console.log("check map user", user)
                             return (
                                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                                    <div style={{ color: 'yellow', paddingTop: '50px' }}>My name's {user.name} </div>
+                                    <div>My name's {user.name} </div>
                                     <div>My age's {user.age} </div>
                                     <hr />
                                 </div>
