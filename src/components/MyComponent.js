a4754cd81e027a225f436f96de20ecffade1456d
Minh Dương <dnminh201103@gmail.com>
2025-09-11 08:45:02 +0700

    video 22

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,53 +3,15 @@
 // function component
 import { eventWrapper } from "@testing-library/user-event/dist/utils";
 import React from "react";
+import UserInfor from "./UserInfor";
 
 class MyComponent extends React.Component {
 
-    state = {
-        name: 'Minh',
-        age: 22,
-        address: 'Hà Nội'
-    }
-
-    handleClick = (event) => {
-        console.log(">>Click me my button")
-        // console.log("random", Math.floor((Math.random() * 100) + 1))
-        this.setState({
-            name: 'Minh Bo',
-            age: Math.floor((Math.random() * 100) + 1),
-        })
-
-    }
-
-    handleOnMouseOver = (event) => {
-        console.log(event.pageX)
-    }
-
-    handleOnChangeInput = (event) => {
-        this.setState ({
-            name: event.target.value
-        })
-
-    }
-
-    handleOnSubmit = (event) => {
-        event.preventDefault()
-        console.log(this.state)
-    }
     // JSX
     render() {
         return (
             <div>
-                My name is {this.state.name} and i'm {this.state.age}
-                <form onSubmit={(event) => this.handleOnSubmit(event)}>
-                    <input
-                        value={this.state.name}
-                        type="text"
-                        onChange={(event) => this.handleOnChangeInput(event)}
-                    />
-                    <button>Submit</button>
-                </form>
+                <UserInfor />
             </div>
         );
     }
