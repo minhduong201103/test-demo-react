f5b98c935ed69d9a513e6b55575bae026209837a
Minh Dương <dnminh201103@gmail.com>
2025-09-14 10:09:20 +0700

    video 49

--- src/index.js
+++ src/index.js
@@ -18,9 +18,10 @@ root.render(
   <Provider store={store}>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="users" element={<User />} />
-        <Route path="admins" element={<Admin />} />
+        <Route path="/" element={<App />} >
+          <Route path="users" element={<User />} />
+          <Route path="admins" element={<Admin />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </Provider >
