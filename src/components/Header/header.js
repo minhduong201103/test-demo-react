40671e52522549fe93ffe9f7e793c1246cc56b64
Minh Dương <dnminh201103@gmail.com>
2025-09-14 09:21:40 +0700

    video 47

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -7,22 +7,19 @@ const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">Hỏi dân IT</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
+                        <Nav.Link href="#link">User</Nav.Link>
+                        <Nav.Link href="#link">Admin</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <NavDropdown title="Settings" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Log out</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
