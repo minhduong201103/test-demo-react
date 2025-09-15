018d795f6ffe10991c6f67ffbf6c950bbf715a35
Minh Dương <dnminh201103@gmail.com>
2025-09-15 22:21:43 +0700

    video 54

--- src/components/Admin/SideBar.js
+++ src/components/Admin/SideBar.js
@@ -1,7 +1,18 @@
-const SideBar = () => { 
-    return(
+import 'react-pro-sidebar/dist/css/style.css';
+import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+
+const SideBar = () => {
+    return (
         <div>
-            SideBar
+            <ProSidebar>
+                <Menu iconShape='square'>
+                    <MenuItem icon={<FaGem />}>Dashboard</MenuItem>
+                    <SubMenu title="components" icon={<FaHeart />}>
+                        <MenuItem>Component 1</MenuItem>
+                        <MenuItem>Component 2</MenuItem>
+                    </SubMenu>
+                </Menu>
+            </ProSidebar>;
         </div>
     )
 }
